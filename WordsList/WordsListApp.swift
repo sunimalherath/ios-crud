ab16abae501b5e622dba199bcb120236a7025e7e
Sunimal Herath <sunimal@live.com>
2023-04-09 16:04:01 +1000

    Initial Commit

---
+++ WordsList/WordsListApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordsListApp.swift
+//  WordsList
+//
+//  Created by Sunimal Herath on 9/4/2023.
+//
+
+import SwiftUI
+
+@main
+struct WordsListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
